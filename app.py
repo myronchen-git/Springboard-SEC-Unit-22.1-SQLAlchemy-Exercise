d3035be43f6ce248d5abf816f02e046d8a871182
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-02-09 14:09:37 -0800

    chore: set up app and HTML infrastructure

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 """Blogly application."""
 
 from flask import Flask
-from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import User, connect_db, db
+
+# --------------------------------------------------
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 connect_db(app)
+app.app_context().push()
 db.create_all()
+
+app.config["SECRET_KEY"] = "secret"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+debug = DebugToolbarExtension(app)
+
+# ==================================================
