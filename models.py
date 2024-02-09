69d5ef0b79dbe7127ce6f469688eaf24acc4e8ad
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-02-09 11:41:26 -0800

    feat: create user model

--- models.py
+++ models.py
@@ -1,1 +1,34 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User for Blogly."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text)
+
+    def __repr__(self):
+        """Show info about user."""
+
+        return (
+            f"<User("
+            f"id={self.id}, "
+            f"first_name={self.first_name}, "
+            f"last_name={self.last_name}, "
+            f"image_url={self.image_url})>"
+        )
